b0334e052ff813b00a3aa2d96eaa9a1fbb69cda2
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-08-03 16:59:04 -0700

    use functioning dimensions handling

--- R/dimensions.R
+++ R/dimensions.R
@@ -1,45 +1,37 @@
-#' Given a dimensions string, returns a tidy table with values, units, and dimension types.
+#' Create a tidy table of extracted dimensions
 #'
-#' @param s Character. Dimension strings to parse.
-#'
-#' @return A dataframe
+#' @return A data frame with an id column for joining to original table,
 #'
 #' @import stringr
-#' @importFrom rematch2 re_match_all bind_re_match
 #'
 #' @export
-parse_dimensions <- function(s) {
-  extracted_dimensions <- general_dimension_extraction(s)
-
-
-}
-
-general_dimension_extraction <- function(s) {
-  re_match_all(s,
+general_dimension_extraction <- function(df, dimcol, idcol) {
+  df <- df[which(!is.na(df[[dimcol]])),]
+  tryd <- rematch2::re_match_all(df[[dimcol]],
                # Find any combo of acceptable
                # value/unit/dimension chars IFF there is
                # at least one digit in the bunch
-               "(?<dim>[0-9 '\"/\\.lhwd]*(?:cm)*\\d+[0-9 '\"/\\.lhwd]*(?:cm)*)") %>%
+               pattern = "(?<dim>[0-9 '\"/\\.lhwd]*(?:cm)*\\d+[0-9 '\"/\\.lhwd]*(?:cm)*)") %>%
+    add_column(star_record_no = df[[idcol]]) %>%
     unnest() %>%
     # Extract the dimension marker to its own column, leaving only the dimension value with its unit markers
     mutate(dim = str_replace(dim, "w w", "w")) %>%
     mutate(dimtype = as.factor(str_trim(str_extract(dim, "( [lhwd] | [lhwd]$)\\.?")))) %>%
-    mutate(value_wo_dim = str_trim(str_replace_all(dim, "( [lhwd] | [lhwd]$)\\.?", ""))) %>%
+    mutate(value_wo_dim = str_trim(str_replace_all(dim, c("( [lhwd] | [lhwd]$)\\.?" = "", " +" = " ")))) %>%
     # Pull out mixed & decimal numbers from their unit markers
-    bind_re_match(value_wo_dim, "(?<dim_d1>[0-9\\.\\/]+(?: ?[0-9]\\/[0-9])?)(?<dim_c1>\\D*)(?<dim_d2>[0-9\\.]*(?: ?[0-9]\\/[0-9])?)(?<dim_c2>\\D*)") %>%
+    rematch2::bind_re_match(value_wo_dim, "^(?<dim_d1>[0-9]{1,2}/[0-9]{1,2}|[0-9\\.]+(?: [0-9]{1,2}/[0-9]{1,2})?)(?<dim_c1>[^0-9\\./]*)(?<dim_d2>[0-9]{1,2}/[0-9]{1,2}|[0-9\\.]*(?: [0-9]{1,2}/[0-9]{1,2})?)(?<dim_c2>[^0-9\\./]*)") %>%
     mutate_at(vars(dim_d1, dim_c1, dim_d2, dim_c2), funs(na_if(str_trim(.), ""))) %>%
     mutate_at(vars(dim_d1, dim_d2), funs(parsed = parse_fraction)) %>%
     mutate_at(vars(dim_c1, dim_c2), as.factor)
 }
 
-general_parse_fraction <- function(df) {
+replace_dimensions <- function(df) {
   df %>%
-    group_by(star_record_no) %>%
-    mutate_at(vars(dim_c1, dim_c2), funs(first = if_else(is.na(first(.)), last(.), first(.)))) %>%
-    ungroup() %>%
-    mutate(
-      dim_c1 = if_else(is.na(dim_c1) & !is.na(dim_d1), dim_c1_first, dim_c1),
-      dim_c2 = if_else(is.na(dim_c2) & !is.na(dim_d2), dim_c2_first, dim_c2)) %>%
-    ungroup() %>%
-    mutate(dim_value = compile_inches(dim_d1, dim_c1, dim_d2, dim_c2))
+    mutate(dimensions = case_when(
+      catalog_project == "British" ~ str_replace_all(dimensions, british_dimension_replacements),
+      TRUE ~ dimensions
+    ))
 }
+
+frd <- general_dimension_extraction(french_sales_dim_mod, dimcol = "new_dims_3", idcol = "star_record_no")
+grmd <- general_dimension_extraction(filter(rsc, str_detect(catalog_number, "^D")), "dimensions", "star_record_no")
