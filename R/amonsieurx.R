99a0e1d2a5ccd0240bea708adc91575730dd9d61
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-09-19 17:54:31 -0700

    generalize amonsierux to run on all price numbers

--- R/amonsieurx.R
+++ R/amonsieurx.R
@@ -5,11 +5,15 @@
 #'
 #' @return Converted vector
 #' @export
-amonsieurx <- function(s, to_number = FALSE) {
+amonsieurx <- Vectorize(function(s, to_number = FALSE) {
   l <- tolower(s)
   e <- stringr::str_extract(l, "[amonsieurx]*")
-  r <- stringr::str_replace_all(e, c("a" = "1", "m" = "2", "o" = "3", "n" = "4", "s" = "5", "i" = "6", "e" = "7", "u" = "8", "r" = "9", "x" = "0"))
-  if (to_number)
-    return(as.numeric(r))
-  r
-}
+  if (e != "" | is.na(e)) {
+    r <- stringr::str_replace_all(e, c("a" = "1", "m" = "2", "o" = "3", "n" = "4", "s" = "5", "i" = "6", "e" = "7", "u" = "8", "r" = "9", "x" = "0"))
+    if (to_number)
+      return(as.numeric(r))
+    return(r)
+  } else {
+    s
+  }
+})
