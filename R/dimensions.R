07c9fc7ccb85b92bc55eda137d6442cd11e970d0
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-10-12 09:28:59 -0700

    properly capture dimension type characters when they are at start of line

--- R/dimensions.R
+++ R/dimensions.R
@@ -16,8 +16,8 @@ general_dimension_extraction <- function(df, dimcol, idcol) {
     unnest() %>%
     # Extract the dimension marker to its own column, leaving only the dimension value with its unit markers
     mutate(dim = str_replace(dim, "w w", "w")) %>%
-    mutate(dimtype = as.factor(str_trim(str_extract(dim, "( [lhwd] | [lhwd]$)\\.?")))) %>%
-    mutate(value_wo_dim = str_trim(str_replace_all(dim, c("( [lhwd] | [lhwd]$)\\.?" = "", " +" = " ")))) %>%
+    mutate(dimtype = as.factor(str_trim(str_extract(dim, "(^[lhwd] | [lhwd] | [lhwd]$)\\.?")))) %>%
+    mutate(value_wo_dim = str_trim(str_replace_all(dim, c("(^[lhwd] | [lhwd] | [lhwd]$)\\.?" = "", " +" = " ")))) %>%
     # Pull out mixed & decimal numbers from their unit markers
     rematch2::bind_re_match(value_wo_dim, "^(?<dim_d1>[0-9]{1,2}/[0-9]{1,2}|[0-9\\.]+(?: [0-9]{1,2}/[0-9]{1,2})?)(?<dim_c1>[^0-9\\./]*)(?<dim_d2>[0-9]{1,2}/[0-9]{1,2}|[0-9\\.]*(?: [0-9]{1,2}/[0-9]{1,2})?)(?<dim_c2>[^0-9\\./]*)") %>%
     mutate_at(vars(dim_d1, dim_c1, dim_d2, dim_c2), funs(na_if(str_trim(.), ""))) %>%
