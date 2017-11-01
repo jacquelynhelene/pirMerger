60b8705fbc6742759cfd96eccd5d18586ac9a099
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-11-01 16:28:17 -0700

    commit remake file of raw_data.yml to secret vault

--- data-raw/refresh_data_definitions.R
+++ data-raw/refresh_data_definitions.R
@@ -1,1 +1,9 @@
-secret::update_secret("working_dict", value = paste0(readLines("data_definitions.yml"), collapse = "\n"))
+refresh_definitions <- function() {
+  current_raw_data <- readLines("data_definitions.yml")
+  secret::update_secret("raw_data", value = current_raw_data, vault = "secret")
+}
+
+write_definitions <- function() {
+  raw_data_text <- secret::get_secret("raw_data", vault = "secret")
+  writeLines(raw_data_definitions, "raw_data.yml")
+}
