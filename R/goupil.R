dceaffb53f37d72a80a898ebb3e192a81b7fa34c
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-11-04 22:21:07 -0700

    break out relational tables for goupil
    closes #58

--- R/goupil.R
+++ R/goupil.R
@@ -1,33 +1,31 @@
-#' Produce Goupil table from raw table.
-#'
-#' This is a two-stage process. First, the raw, mostly denormalized exports are
-#' read in, and then normalized into a series of tables. Secondly, IDs for
-#' artists, buyers, sellers, objects, and transaction & inventory events are
-#' generated and attached to the reamining core `goupil` table.
-#'
-#' @param source_dir Path where source RDS files are found
-#' @param target_dir Path where resulting RDS files are saved
-#'
-#' @importFrom rematch2 re_match bind_re_match
-#'
-#' @export
-produce_goupil <- function(source_dir, target_dir) {
-  raw_goupil <- get_data(source_dir, "raw_goupil")
+produce_goupil_ids <- function(raw_goupil) {
+  raw_goupil %>%
+    mutate_at(vars(star_record_no, contains("stock_book"), page_number, row_number, contains("_date_")), funs(as.integer)) %>%
+    select(-persistent_uid, -original_file_name)
+}
 
-  goupil <- raw_goupil %>%
-    select(-star_record_no)
+produce_goupil <- function(goupil_with_ids) {
+  goupil_with_ids %>%
+    select(-(stock_book_no_1:stock_book_row_15)) %>%
+    select(-(artist_name_1:artist_ulan_id_2)) %>%
+    select(-(buyer_name_1:buyer_ulan_id_2)) %>%
+    select(-(previous_owner_1:previous_owner_2))
+}
 
-  # --- The following functions break out tables for multiply-ocurring fields,
-  # writing them as normalized tables with keys to join back on to the core
-  # `goupil` table ---
-  message("- Extracting goupil _artists")
-  goupil_artists <- norm_vars(goupil, base_names = c("artist_name", "art_authority", "nationality", "attribution_mod", "star_rec_no", "artist_ulan_id"), n_reps = 2, idcols = "star_record_no") %>%
-    rename(artist_star_record_no = star_rec_no) %>%
+produce_goupil_stock_book_nos <- function(goupil) {
+  norm_vars(goupil, base_names = c("stock_book_no", "stock_book_goupil_no", "stock_book_page", "stock_book_row"), n_reps = 15, idcols = "star_record_no")
+}
+
+produce_goupil_artists <- function(goupil) {
+  norm_vars(goupil, base_names = c("artist_name", "art_authority", "nationality", "attribution_mod", "star_rec_no", "artist_ulan_id"), n_reps = 2, idcols = "star_record_no") %>%
     # Join ulan ids to this list
-    rename(artist_authority = art_authority, artist_nationality = nationality, artist_attribution_mod = attribution_mod)
-  goupil <- goupil %>%
-    select(-(artist_name_1:artist_ulan_id_2))
-  save_data(target_dir, knoedler_artists)
+    rename(artist_star_record_no = star_rec_no, artist_authority = art_authority, artist_nationality = nationality, artist_attribution_mod = attribution_mod)
+}
 
-  save_data(target_dir, goupil)
+produce_goupil_buyers <- function(goupil) {
+  norm_vars(goupil, base_names = c("buyer_name", "buyer_loc", "buyer_mod", "buy_auth_name", "buy_auth_mod", "buyer_ulan_id"), n_reps = 2, idcols = "star_record_no")
+}
+
+produce_goupil_previous_owners <- function(goupil) {
+  norm_vars(goupil, base_names = c("previous_owner"), n_reps = 2, idcols = "star_record_no")
 }
